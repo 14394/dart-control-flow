47dacb68ea2a8971bc317583b634535692a42e8a
Collins Wegulo <wegulocollins77@gmail.com>
2024-03-12 20:03:24 +0300

    first commit

---
+++ test/dart_control_flow_assignment_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_control_flow_assignment/dart_control_flow_assignment.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
